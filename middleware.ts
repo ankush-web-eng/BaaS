35dd9de07ae1d6c92751acbf9aec5e918da10d9c
Ankush <deshwalankush23@gmail.com>
2024-09-13 14:06:48 +0530

    middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -19,11 +19,11 @@ export default async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 
-    // if (
-    //     !token && url.pathname.startsWith('/dashboard')
-    // ) {
-    //     return NextResponse.redirect(new URL('/signin', request.url));
-    // }
+    if (
+        !token && url.pathname.startsWith('/dashboard')
+    ) {
+        return NextResponse.redirect(new URL('/signin', request.url));
+    }
 
     return NextResponse.next();
 }
