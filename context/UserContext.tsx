51714643fa7ed9ae16e07018ba8778d444613e95
Ankush <deshwalankush23@gmail.com>
2024-09-14 10:12:59 +0530

    Context api enhanced

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -2,7 +2,8 @@
 
 import { User } from "@/types/User";
 import axios from "axios";
-import { createContext, useContext, useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 
 interface UserContextProps {
     user: User | null;
@@ -14,19 +15,21 @@ const userContext = createContext<UserContextProps | null>(null);
 const UserContextProvider = ({ children }: { children: React.ReactNode }) => {
 
     const [user, setUser] = useState<User | null>(null);
+    const { data: session } = useSession();
 
-    const getUser = async () => {
+    const getUser = useCallback(async () => {
         try {
-            const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user`);
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user?email=${session?.user?.email}`);
             setUser(res.data);
+            console.log(res.data);
         } catch (error) {
             console.error(error);
         }
-    }
+    }, [session?.user?.email]);
 
     useEffect(() => {
         getUser();
-    }, []);
+    }, [getUser]);
 
     const updateUser = () => {
         getUser();
