562f8ecda0795642d7b7428c6b4fd8a1fbd9ac5d
Ankush <deshwalankush23@gmail.com>
2024-09-15 10:03:38 +0530

    Menu dropdown added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import ApiKey from "@/components/dash/apikey";
 import CloudinaryCredentials from "@/components/dash/cloudinary";
 import MailCredentials from "@/components/dash/mail";
+import Menu from "@/components/layout/Header/Menu";
 import { Metadata } from "next";
 
 export const metadata : Metadata = {
@@ -13,6 +14,7 @@ export default function Page() {
 
     return (
         <div className="min-h-screen bg-[#0f0a39] flex max-md:flex-col max-md:space-y-3 justify-center items-center space-x-5">
+            <Menu />
             <ApiKey />
             <CloudinaryCredentials />
             <MailCredentials />
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,8 +15,6 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        tertiary:
-          "bg-secondary text-secondary-foreground rounded-xl shadow-xl text-gray-600",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -38,7 +36,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
